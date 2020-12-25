7a254237ceb3be68e45e9116ae0246d16ab3433d
Max Wolf <wolf@zirkeltraining.org>
2020-12-25 22:07:29 +0100

    buttons

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,63 +2,86 @@ function showStatistics(selection) {
     clear()
     const statByType = calcByType(selection)
     getContainer().appendChild(createStatTable('by Type', 'Looks like the selection is empty.', statByType))
-  }
-  
-  function clear() {
+}
+
+function clear() {
     const elements = getContainer().getElementsByClassName('stat-list__table')
     for (let i = 0; i < elements.length; i++) {
-      elements.item(i).remove()
+        elements.item(i).remove()
     }
-  }
-  
-  function getContainer() {
+}
+
+function getContainer() {
     return document.getElementById('stat-container')
-  }
-  
-  function createStatTable(title, emptyText, data) { 
+}
+
+function createStatTable(title, emptyText, data) {
     const statView = document.createElement('div')
     statView.className = 'stat-list__table'
-  
+
     const titleView = document.createElement('div')
     titleView.className = 'stat-list__title'
     titleView.innerHTML = `<span>${title}</span>`
     statView.appendChild(titleView)
-  
+
     if (data.size === 0) {
-      const emptyView = document.createElement('div')
-      emptyView.className = 'stat-list__empty'
-      emptyView.innerText = emptyText
-      statView.appendChild(emptyView)
+        const emptyView = document.createElement('div')
+        emptyView.className = 'stat-list__empty'
+        emptyView.innerText = emptyText
+        statView.appendChild(emptyView)
     } else {
-      data.forEach((value, key) => {
-        let itemView = document.createElement('div')
-        itemView.className = 'stat-list__item'
-        itemView.innerHTML =
-          `<span class="stat-list__item-name">${key.toLowerCase()}</span>` +
-          `<span class="stat-list__item-value">${value}</span>`
-        statView.appendChild(itemView)
-      })
+        data.forEach((value, key) => {
+            let itemView = document.createElement('div')
+            itemView.className = 'stat-list__item'
+            itemView.innerHTML =
+                `<span class="stat-list__item-name">${key.toLowerCase()}</span>` +
+                `<span class="stat-list__item-value">${value}</span>`
+            statView.appendChild(itemView)
+        })
     }
     return statView
-  }
-  
-  function calcByType(widgets) {
+}
+
+function calcByType(widgets) {
     return countBy(widgets, (a) => a.type)
-  }
-  
-  function countBy(list, keyGetter) {
+}
+
+function countBy(list, keyGetter) {
     const map = new Map()
     list.forEach((item) => {
-      const key = keyGetter(item)
-      const count = map.get(key)
-      map.set(key, !count ? 1 : count + 1)
+        const key = keyGetter(item)
+        const count = map.get(key)
+        map.set(key, !count ? 1 : count + 1)
     })
     return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
-  }
-  
-  miro.onReady(() => {
+}
+
+function showAllCards() {
+    console.log("getting Cards")
+    //await a=miro.board.widgets.get({type:'CARD', title:'<p>s</p>'})
+    cards = miro.board.widgets.get({type:'CARD'})
+    console.log (cards)
+}
+
+function showAllUsers() {
+    console.log("getting Users with oauth token from local storage " + localStorage.getItem("MiroOA"))
+    fetch("https://api.miro.com/v1/teams/3074457352877196176/user-connections?limit=10&offset=0", {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + localStorage.getItem("MiroOA")
+        }
+    })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+miro.onReady(() => {
     miro.addListener('SELECTION_UPDATED', (e) => {
-      showStatistics(e.data)
+        showStatistics(e.data)
     })
     miro.board.selection.get().then(showStatistics)
-  })
+})
